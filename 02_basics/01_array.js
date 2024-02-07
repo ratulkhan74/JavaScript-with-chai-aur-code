13cc7bbf0bc93cc5b3a5fece72b34c7096c6a397
ratulkhan74 <ratul.khan503040@gmail.com>
2024-02-08 03:57:00 +0600

    added new folder

---
+++ 02_basics/01_array.js
@@ -0,0 +1,24 @@
+// Array
+// let myArr = [1,2,3,4,5,6,7,8,9]
+//myArr.push('Ratul') // push() method will add element in an array from last
+//myArr.pop() // pop() method will remove the last element from in an array
+//myArr.unshift('sazib') // unshift() will add element in an arrya from start
+//myArr.shift() // shift() will remove the first element from in an array
+// console.log(myArr);
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+let arrSlice = fruits.slice(0, 3) // Do not change the main array
+// console.log(arrSlice);
+// console.log(fruits);
+const fruits2 = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+let arrSplice = fruits2.splice(1, 3) // modify the main array and create a new array
+// console.log(arrSplice);
+// console.log(fruits2);
+
+// let nestedArray = [1,2,[3,4,[5,6],7,8,[9]]]
+const fruits3 = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// let newSpliced = fruits3.toSpliced(0,2);
+// console.log(newSpliced);
+// console.log(nestedArray.flat(Infinity));
+//console.log(fruits3.includes("Lemon")); // includes() method check element in an array
+//console.log(fruits3.indexOf("Orange")); // indexOf() method will return the index number of the element in an array
+
