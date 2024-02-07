28d97b2e8f5376dd080ea7e568ad4b12371c4128
ratulkhan74 <ratul.khan503040@gmail.com>
2024-02-08 02:36:43 +0600

    adding date fiel

---
+++ 01_basics/07_date.js
@@ -0,0 +1,10 @@
+// Dates
+let date = new Date()
+let currentDate = date.getDate()
+let month = date.getMonth() +1
+let year = date.getFullYear()
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+let fullDate = `${currentDate}/${month}/${year}`
+console.log(fullDate);
