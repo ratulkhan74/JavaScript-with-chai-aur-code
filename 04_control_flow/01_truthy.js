f1eb7d763f47ad02d19e583162a3076866b605e2
ratulkhan74 <ratul.khan503040@gmail.com>
2024-02-10 23:04:00 +0600

    adding new folder control flow

---
+++ 04_control_flow/01_truthy.js
@@ -0,0 +1,45 @@
+//const usersEmail = []
+
+//console.log(usersEmail.length); // checking array length
+
+//const arrayLength = usersEmail.length // Assigning array length in a variable
+
+// if (Array.isArray(usersEmail) && arrayLength === 0) {
+//     console.log("Array is empty");
+// }
+// Custom function for checking array
+// function is_array(array) {
+//     return Array.isArray(array)
+// }
+
+// if (is_array(usersEmail)) {
+//     console.log(`This is an array`);
+// }
+
+// Checking empty object
+
+// const emptyObject = {}
+// const objLength = Object.keys(emptyObject).length // checking object length
+// if (objLength === 0) {
+//     console.log("The object is empty");
+// }
+
+// Custom object checker
+// function isObject(object) {
+//     return Object.keys(object)
+// }
+// const objectLength = isObject(emptyObject).length // checking object length with custom object checker
+// if (objectLength === 0) {
+//     console.log("The object is empty with custom checker");
+// }
+
+
+// Falsy valus
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy value
+// "0", 'false', " ", [], {}, function(){}
+
+// Note: " ", In an empty quotation if get an space it will consider as a truthy value
+
+// Note: In a quotation if get any value such as 0, false or space (space will be consider as a value in an empty quotation) then it is a truthy value
