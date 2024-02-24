5c5820fd8b8dfa1e7f9829b85441b4b00e7dee76
ratulkhan74 <ratul.khan503040@gmail.com>
2024-02-25 04:25:27 +0600

    learnign events

--- 08_events/events.js
+++ 08_events/events.js
@@ -9,24 +9,9 @@ const getEventID = document.getElementById('colorful-cacarons');
 //     console.log(e);
 // }, false); // In addEventListener('type',callBack Function, true/false) there is a third parameter
 
-/**
- * ---------------------------
- * Note: need to study in details on:
- * ---------------------------
- * type, timestamp, prevantDefault
- * ---------------------------
- * target, toElement, srcElement, currentElement
- * ---------------------------
- * clientX, clientY, screenX, screenY
- * ---------------------------
- * Keyboard special keys:
- * altKey, shiftKey, ctrlKey, keyCode
- * ---------------------------
-*/
-
 // Event propagation
 
-// Parent 
+// Parent
 // document.getElementById('lists').addEventListener('click', function (e) {
 //     console.log('clicked on parent element (UL is the parent element)');
 // }, false) // Event bubbling mood turn on by adding false (by default it is false) // bubbling will appear the child element first after that parent element
@@ -54,13 +39,62 @@ const getEventID = document.getElementById('colorful-cacarons');
 
 
 // Deleting image
-const imagesList = document.querySelector('#lists');
-imagesList.addEventListener('click', function (e) {
-    const imgTagName = e.target.tagName;
-    if (imgTagName === "IMG") {
-        let listItem = e.target.parentNode; // getting parent for targeted element (here the targeted element is image li is the patent element and we are removing the parent (li) element)
-        listItem.remove(); // Removing the parent element of the targeted element
-        console.log(`we removed ${e.target.alt}`);
-        // listItem.parentNode.removeChild(listItem); // removing child item from parent (ul) element li is the child element
-    }
+// const imagesList = document.querySelector('#lists');
+// imagesList.addEventListener('click', function (e) {
+//     const imgTagName = e.target.tagName;
+//     if (imgTagName === "IMG") {
+//         let listItem = e.target.parentNode; // getting parent for targeted element (here the targeted element is image li is the patent element and we are removing the parent (li) element)
+//         listItem.remove(); // Removing the parent element of the targeted element
+//         console.log(`we removed ${e.target.alt}`);
+//         // listItem.parentNode.removeChild(listItem); // removing child item from parent (ul) element li is the child element
+//     }
+// })
+
+// document.getElementById('btn').addEventListener('click',function(e){
+// document.getElementById('btn').addEventListener('dblclick',function(e){
+// document.getElementById('btn').addEventListener('mousedown',function(e){
+// document.getElementById('btn').addEventListener('mouseup',function(e){
+// document.getElementById('btn').addEventListener('contextmenu',function(e){
+// document.getElementById('btn').addEventListener('mouseout',function(e){
+// document.getElementById('btn').addEventListener('mousewheel',function(e){
+// document.getElementById('btn').addEventListener('mouseover', function (e) {
+// document.getElementById('btn').addEventListener('keydown', function (e) {
+// document.getElementById('btn').addEventListener('keyup', function (e) {
+// document.getElementById('btn').addEventListener('keypress', function (e) {
+// document.getElementById('btn').addEventListener('scroll', function (e) {
+
+// document.getElementById('btn').addEventListener('click', function (e) {
+//     console.log(e.timeStamp / 1000);
+// });
+
+/**
+ * ---------------------------
+ * Note: need to learn in details on:
+ * ---------------------------
+ * type, timestamp, prevantDefault // done
+ * ---------------------------
+ * target, toElement, srcElement, currentElement
+ * ---------------------------
+ * clientX, clientY, screenX, screenY
+ * ---------------------------
+ * Keyboard special keys:
+ * altKey, shiftKey, ctrlKey, keyCode
+ * ---------------------------
+*/
+
+// document.getElementById('btn').addEventListener('click',function(e){
+//     console.log(e.target);
+// })
+
+// Event type target
+// document.querySelector('ul').addEventListener('click', function (e) {
+//     if (e.target.tagName === "LI") {
+//         e.target.style.color = '#ff0000'
+//     }
+// })
+
+document.querySelector('#container').addEventListener('mousemove',function(e){
+    document.getElementById('container').innerHTML = `Move your mouse to see the container position: ScreenX: ${e.screenX} and ScreenY: ${e.screenY} also clientX: ${e.clientX} clientY: ${e.clientY}`;
 })
+
+
